531bb1090cc62796b05786879a73bbca22e39d6d
Luigi Morel <morelmiles@gmail.com>
2022-12-06 12:49:05 +0300

    fix API link
    Signed-off-by: Luigi Morel <morelmiles@gmail.com>

--- utils/currency.go
+++ utils/currency.go
@@ -17,7 +17,7 @@ type CurrencyConvert struct {
 func ConvertMoney() string {
 	apiKey := os.Getenv("MONEY_API_KEY")
 
-	url := "https://api.apilayer.com/fixer/convert?to=UGX&from=GBP&amount=1"
+	url := "https://api.apilayer.com/currency_data/convert?to=UGX&from=USD&amount=1"
 
 	client := &http.Client{}
 	request, err := http.NewRequest("GET", url, nil)
@@ -43,12 +43,12 @@ func ConvertMoney() string {
 	if err := json.Unmarshal(body, &result); err != nil {
 		panic(err)
 	}
-
 	convertResultToInt := int(result.Result)
+
 	convertResultToString := strconv.Itoa(convertResultToInt)
 
 	currentDate := time.Now()
-	formatDate := currentDate.Format("Mon September, 15:04:05 PM") + "\n"
+	formatDate := currentDate.Format("02 January 2006, 15:04:05 PM") + "\n"
 
 	version := LogVersion()
 
